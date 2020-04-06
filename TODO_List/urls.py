e7c09c4c33aabecbf2e3c4f556eb2d445152714a
Miao HaoYu <adammiaohaoyu@163.com>
2020-04-06 19:30:38 +0800

    'N'

--- TODO_List/urls.py
+++ TODO_List/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('todolist.urls')),
+    path('', include(('todolist.urls', 'todolist'), namespace="todolist")),
     path('admin/', admin.site.urls),
 ]
